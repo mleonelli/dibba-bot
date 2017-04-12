4433f335b3e5a8423688f3e5894515e2014fd521
Mauro Leonelli <mleonelli@gmail.com>
2017-04-12 14:47:12 +0200

    Version without setWebhook

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-//var TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api');
 var express = require('express');
 const token = 't278945464:AAFi4blEbA48qw44qP_Ixrzl3qvcIsJbDok';
 
 const options = {
   webHook: {
-    port: 80,
+    port: 443,
     key: `./40413500-dibba-bot.herokuapp.com.key`,  // Path to file with PEM private key
     cert: `./40413500-dibba-bot.herokuapp.com.cert`  // Path to file with PEM certificate
   }
 };
 // This URL must route to the port set above (i.e. 443)
 const url = 'https://dibba-bot.herokuapp.com/';
-//const bot = new TelegramBot(token, options);
+const bot = new TelegramBot(token, options);
 
 // This informs the Telegram servers of the new webhook.
-//bot.setWebHook(`${url}/bot${token}`, {
-//  certificate: options.webHook.cert,
-//});
+bot.setWebHook(`${url}/bot${token}`, {
+  certificate: options.webHook.cert,
+});
 
 
 // Just to ping!
-//bot.on('message', function onMessage(msg) {
-//  bot.sendMessage(msg.chat.id, 'I am alive!');
-//});
-
-var app = express();
-
-app.get('/', function(req, res){
-  res.send('Hello!');
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!')
-})
+//var app = express();
+
+// app.get('/', function(req, res){
+//   res.send('Hello!');
+// });
+
+// app.listen(4000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
