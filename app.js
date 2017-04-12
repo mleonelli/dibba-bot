a7d7e923c904d89b1aa0ca0d4170c552e643a996
Mauro Leonelli <mleonelli@gmail.com>
2017-04-12 14:59:42 +0200

    New Test

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 var TelegramBot = require('node-telegram-bot-api');
-var express = require('express');
-const token = 't278945464:AAFi4blEbA48qw44qP_Ixrzl3qvcIsJbDok';
+
+/**
+ * This example demonstrates setting up webhook
+ * on the Heroku platform.
+ */
+
+
+const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 
 const options = {
   webHook: {
-    port: 443,
-    key: `./40413500-dibba-bot.herokuapp.com.key`,  // Path to file with PEM private key
-    cert: `./40413500-dibba-bot.herokuapp.com.cert`  // Path to file with PEM certificate
+    // Port to which you should bind is assigned to $PORT variable
+    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+    port: process.env.PORT
+    // you do NOT need to set up certificates since Heroku provides
+    // the SSL certs already (https://<app-name>.herokuapp.com)
+    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
-// This URL must route to the port set above (i.e. 443)
-const url = 'https://dibba-bot.herokuapp.com/';
-const bot = new TelegramBot(token);
+// Heroku routes from port :443 to $PORT
+// Add URL of your app to env variable or enable Dyno Metadata
+// to get this automatically
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
+
 
 // This informs the Telegram servers of the new webhook.
-//bot.setWebHook(`${url}/bot${token}`, {
-//  certificate: options.webHook.cert,
-//});
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
 // Just to ping!
 bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
-
-//var app = express();
-
-// app.get('/', function(req, res){
-//   res.send('Hello!');
-// });
-
-// app.listen(4000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
