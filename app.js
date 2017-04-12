5dafb402ffe2bafb26ec3131215e698fff26f4aa
Mauro Leonelli <mleonelli@gmail.com>
2017-04-12 13:39:01 +0200

    Test3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var TelegramBot = require('node-telegram-bot-api');
+//var TelegramBot = require('node-telegram-bot-api');
+var express = require(express)
 const token = 't278945464:AAFi4blEbA48qw44qP_Ixrzl3qvcIsJbDok';
 
 const options = {
@@ -10,16 +11,21 @@ const options = {
 };
 // This URL must route to the port set above (i.e. 443)
 const url = 'https://dibba-bot.herokuapp.com/';
-const bot = new TelegramBot(token, options);
-
+//const bot = new TelegramBot(token, options);
 
 // This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${token}`, {
-  certificate: options.webHook.cert,
-});
+//bot.setWebHook(`${url}/bot${token}`, {
+//  certificate: options.webHook.cert,
+//});
 
 
 // Just to ping!
-bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
+//bot.on('message', function onMessage(msg) {
+//  bot.sendMessage(msg.chat.id, 'I am alive!');
+//});
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello!');
 });
